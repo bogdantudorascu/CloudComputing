fc10acd0859552e1f8aff7f40634f5295d65bedf
Trung Nguyen <thanhtrung674@gmail.com>
2018-05-03 00:28:58 +0100

    fix authentication

--- GiveItAGo/WEB-INF/src/com/sheffield/giveitago/filters/AuthenticationFilter.java
+++ GiveItAGo/WEB-INF/src/com/sheffield/giveitago/filters/AuthenticationFilter.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -16,7 +17,7 @@ import javax.servlet.http.HttpSession;
  */
 
 public class AuthenticationFilter implements Filter {
-	
+	private ServletContext context;
     /**
      * Default constructor. 
      */
@@ -28,7 +29,7 @@ public class AuthenticationFilter implements Filter {
 	 * @see Filter#destroy()
 	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
+		
 	}
 
 	/**
@@ -39,20 +40,21 @@ public class AuthenticationFilter implements Filter {
 		HttpServletResponse res = (HttpServletResponse) response;
 		
 		HttpSession session = req.getSession(false);
-		// Request is invalid if there is no session or no username attribute in session
-		if (session == null || session.getAttribute("username") == null) {
+		String usernameCtx = (String)this.context.getAttribute("username");
+		// Request is invalid if no username attribute in Context or there is no session or no username attribute in session
+		if (usernameCtx == null || session == null || session.getAttribute("username") == null) {
 			res.sendRedirect("/CloudComputing");
 		} else {
 			// pass the request along the filter chain
 			chain.doFilter(request, response);
 		}
-
 	}
 
 	/**
 	 * @see Filter#init(FilterConfig)
 	 */
 	public void init(FilterConfig fConfig) throws ServletException {
+		this.context = fConfig.getServletContext();
 	}
 
 }
